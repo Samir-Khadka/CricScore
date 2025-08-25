a967ff7f5c481240f4b97b02c9f55dfe915788ea
nbinayak02 <binayak0128@gmail.com>
2025-08-25 08:10:34 +0545

    Fetch and show tournaments name complete

--- server/controllers/viewerController.js
+++ server/controllers/viewerController.js
@@ -1,7 +1,21 @@
-async function handleGetAllTournaments(req, res){
+const Tournament = require("../models/Tournment");
 
+async function handleGetAllTournaments(req, res) {
+  try {
+    const tours = await Tournament.find();
+    if (tours.length === 0) {
+      return res.status(200).json({ message: "No Tournaments", data: [] });
+    }
+    return res
+      .status(200)
+      .json({ message: "Tournaments fetched successfully", data: tours });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Something went wrong while fetching tournaments" });
+  }
 }
 
 module.exports = {
-    handleGetAllTournaments,
-}
+  handleGetAllTournaments,
+};
--- user/src/components/TournamentsCard.jsx
+++ user/src/components/TournamentsCard.jsx
@@ -1,11 +1,21 @@
-const Tournaments = () => {
+import { useNavigate } from "react-router-dom";
 
-    return (
-        <div className="w-fit p-3 border-2 border-gray-400 rounded-2xl">
-            <p>Nepal Premier League</p>
-        </div>
-    );
+const Tournaments = (props) => {
 
-}
+  const navigate = useNavigate();
+
+  const handleNavigate = (to) => {
+    navigate(`/tournament/${to}`);
+  };
+
+  return (
+    <div
+      className="w-fit p-3 border-2 border-gray-400 rounded-2xl hover:bg-blue-400 transition-colors hover:text-white cursor-pointer"
+      onClick={() => {handleNavigate(props.name)}}
+    >
+      <p>{props.name}</p>
+    </div>
+  );
+};
 
 export default Tournaments;
