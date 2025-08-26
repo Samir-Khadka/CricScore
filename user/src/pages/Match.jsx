1af5ec8588ab84fda553d5077fc8df2922a1dfcd
nbinayak02 <binayak0128@gmail.com>
2025-08-26 18:38:53 +0545

    Configured score timeline

--- user/src/pages/Match.jsx
+++ user/src/pages/Match.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import {useState } from "react";
+import TimelineItem from "../components/TimelineItem";
 
 const Match = () => {
   const [selectedTab, setSelectedTab] = useState("com");
@@ -9,6 +10,30 @@ const Match = () => {
     { id: "pt", label: "Points Table" },
   ];
 
+  const timeline = [
+    { over: 1, balls: ["0", "2", "6", "4", "WD", "1", "W"] },
+    { over: 2, balls: ["1", "0", "0", "2", "1", "4"] },
+    { over: 3, balls: ["4NB", "6", "0", "2", "1", "4LB", "W"] },
+    { over: 4, balls: ["6", "WD", "6", "4", "6", "6", "6"]},
+    { over: 5, balls: ["4", "0", "1", "1", "2", "0"] },
+    { over: 6, balls: ["6", "4", "1", "0", "0", "W"] },
+    { over: 7, balls: ["0", "0", "1", "1", "4", "1"] },
+    { over: 8, balls: ["2", "1", "1", "6", "4", "W"] },
+    { over: 9, balls: ["0", "0", "0", "0", "0", "0"] },
+    { over: 10, balls: ["6", "6", "6", "6", "6", "6"] },
+    { over: 11, balls: ["1", "2", "0", "4", "1", "W"] },
+    { over: 12, balls: ["0", "0", "0", "0", "0", "W"] },
+    { over: 13, balls: ["1", "1", "1", "1", "1", "1"] },
+    { over: 14, balls: ["4", "4", "4", "4", "4", "4"] },
+    { over: 15, balls: ["2", "2", "2", "2", "2", "W"] },
+    { over: 16, balls: ["6", "0", "1", "4", "2", "1"] },
+    { over: 17, balls: ["1", "0", "0", "2", "1", "4"] },
+    { over: 18, balls: ["4", "6", "4", "6", "2", "W"] },
+    { over: 19, balls: ["0", "0", "1", "0", "1", "0"] },
+    { over: 20, balls: ["W", "W", "W", "W"] } // The innings ends with the 10th wicket falling mid-over
+];
+
+  timeline.reverse();
   return (
     <div className="w-full flex flex-col items-center justify-center bg-[#f8f9fa]">
       {/* live scorecard */}
@@ -55,6 +80,26 @@ const Match = () => {
             Current Partnership 58(16) <br />
             Current Run Rate 12.4
           </div>
+        </div>
+
+        {/* timeline  */}
+        <div className="mt-10 flex flex-row overflow-x-scroll " style={{"scrollbar-width":"none"}}>
+          {timeline.map((t) => {
+            return t.balls.reverse().map((b, i) => {
+              return (
+                <div className="flex flex-row items-center">
+                  {i === 0 ? (
+                    <p className="text-md font-semibold text-gray-500 px-3">
+                      {t.over}
+                    </p>
+                  ) : (
+                    ""
+                  )}
+                  <TimelineItem result={b} />
+                </div>
+              );
+            });
+          })}
         </div>
 
         {/* other info */}
